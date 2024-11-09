d1bf938862dce581311a8213e8e776babf500087
salab <dimasat15@gmail.com>
2024-11-09 15:55:38 +0900

    add experiments.ipynb

--- trainer.py
+++ trainer.py
@@ -8,16 +8,20 @@ import torch
 import numpy as np
 from datetime import datetime
 from tqdm import tqdm
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import classification_report
+import os
+import time
 
 
 # A function to encapsulate the training loop
-def batch_gd(model, criterion, optimizer, train_loader, test_loader, epochs=200):
+def batch_gd(model, criterion, optimizer, train_loader, test_loader, k, epochs=200):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_losses = np.zeros(epochs)
     test_losses = np.zeros(epochs)
     best_test_loss = np.inf
     best_test_epoch = 0
+    best_model_state = model.state_dict()
+    time_start = time.time()
 
     for it in tqdm(range(epochs)):
 
@@ -58,7 +62,7 @@ def batch_gd(model, criterion, optimizer, train_loader, test_loader, epochs=200)
         test_losses[it] = test_loss
 
         if test_loss < best_test_loss:
-            torch.save(model, './best_val_model_pytorch')
+            best_model_state = model.state_dict()
             best_test_loss = test_loss
             best_test_epoch = it
             print('model saved')
@@ -67,7 +71,17 @@ def batch_gd(model, criterion, optimizer, train_loader, test_loader, epochs=200)
         print(f'Epoch {it + 1}/{epochs}, Train Loss: {train_loss:.4f}, \
           Validation Loss: {test_loss:.4f}, Duration: {dt}, Best Val Epoch: {best_test_epoch}')
 
-    return train_losses, test_losses
+    model.load_state_dict(best_model_state)
+    exp_path = './Experiments/'
+    if not os.path.exists(exp_path):
+        os.makedirs(exp_path)
+    time_stamp = datetime.today().strftime('%Y%m%d_%H%M')
+    model_save_path = os.path.join(exp_path, 'model' + '_K' + k + '_' + time_stamp + '.pth')
+    torch.save(model, model_save_path)
+    duration = time.time() - time_start
+    print('Training completed in {:.0f}m {:.0f}s'.format(duration // 60, duration % 60))
+
+    return model, train_losses, test_losses
 
 
 def evaluate(model, test_loader):
