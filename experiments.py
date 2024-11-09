a12e8d32605571265707d883160cfec63cdd11a8
salab <dimasat15@gmail.com>
2024-11-09 16:01:54 +0900

    add experiments.ipynb

--- experiments.py
+++ experiments.py
@@ -9,9 +9,11 @@ data_path = './data/'
 train, val, test = load_data(data_path)
 
 batch_size = 32
-k = 10
 num_classes = 3
 T = 100
+
+
+k = 10
 
 dataset_train = Dataset(data=train, k=k, num_classes=num_classes, T=T)
 dataset_val = Dataset(data=val, k=k, num_classes=num_classes, T=T)
@@ -22,3 +24,4 @@ val_loader = torch.utils.data.DataLoader(dataset=dataset_val, batch_size=batch_s
 test_loader = torch.utils.data.DataLoader(dataset=dataset_test, batch_size=batch_size, shuffle=False)
 
 print(dataset_train.x.shape, dataset_train.y.shape)
+
